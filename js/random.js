f43ffec2ac2662db3c891ecdefeb6b533cfa5991
tianzixiao <3085438886@qq.com>
2022-11-10 07:41:44 +0800

    Site updated: 2022-11-10 07:41:40

--- js/random.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var page=[
-    "https://github.com/tianzixiao-a"
-];
-    function toRandomPage(){
-        pjax.loadUrl(
-            page[Math.floor(Math.random()*page.length)]
-        )
-    }

add7d67037b4600e00488fbdffdc5f1cd48df89c
tianzixiao <3085438886@qq.com>
2022-11-09 18:15:49 +0800

    Site updated: 2022-11-09 18:15:44

--- js/random.js
+++ js/random.js
@@ -1,6 +1,5 @@
 "use strict";
 var page=[
-    "https://github.com/tianzixiao-a",
-    "https://tzxblog.eu.org"
+    "https://github.com/tianzixiao-a"
 ];
     function toRandomPage(){pjax.loadUrl("/"+page[Math.floor(Math.random()*page.length)])}

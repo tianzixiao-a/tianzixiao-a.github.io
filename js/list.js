e0bef9faa9e5dc22152a044d54a77e1774f06c1b
tianzixiao <3085438886@qq.com>
2022-11-10 18:32:14 +0800

    Site updated: 2022-11-10 18:32:05

--- js/list.js
+++ js/list.js
@@ -18,3 +18,5 @@ function percent() {
   result <= 99 || (result = 99), (btn.innerHTML = result);
   document.getElementById("page-name").innerText = document.title.split(" | 天子笑")[0];
 }
+
+document.getElementById("page-name").innerText = document.title.split(" | 天子笑")[0];

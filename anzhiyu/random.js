d74aa7b8ab98c9a689dfdd2eb6d6e9d75cf16be3
tianzixiao <3085438886@qq.com>
2022-11-10 18:16:43 +0800

    Site updated: 2022-11-10 18:16:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/e59e.html","posts/5f67.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/5f67.html","posts/e59e.html","posts/e59e.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
